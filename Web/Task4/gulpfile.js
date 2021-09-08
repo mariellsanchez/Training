d06424e3511189041306abf1f91cfe9cc61bfd2b
mariellsanchez <mariellsanchez@estudiantec.cr>
2021-09-08 10:01:04 -0600

    Task4 using gulp and footer fixed :)

--- Web/Task4/gulpfile.js
+++ Web/Task4/gulpfile.js
@@ -1,6 +1,15 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-}
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
 
-exports.default = defaultTask
+gulp.task('sass', function() {
+  return gulp.src('./scss/styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+// function defaultTask(cb) {
+//     // place code for your default task here
+//     cb();
+// }
+
+// exports.default = defaultTask
